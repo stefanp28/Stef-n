882275e011a4d836bdba3ae43e3990c2cc9db535
Stefan <stefanp281@gmail.com>
2019-09-17 14:24:23 +0000

    bla

---
+++ Desktop/assignments_i_tima/TileTraveller-master/tile_traveller.py
@@ -0,0 +1,7 @@
+#First we print out the options for which way to go
+#Next write down input for user to enter
+#Print out possible routes, and coninue so inside a loop.
+#At last output, we print Victory. (box 3,1) and the program stops
+#If user inputs invalid directions, program prints out invalid.
+#Re enter correct input to continue. 
+
